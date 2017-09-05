b1f73dd3793128c4193ee4293dbd7717bdcafed7
tovigu <tovigu@gmail.com>
2017-09-05 07:30:05 -0400

    Generic Linked List

--- learnSocket.c
+++ learnSocket.c
@@ -1,3 +1,11 @@
+
+/* Version 0.1
+ * Presently only one client is handled at a time by the threads 
+ * In the present functionality anything sent by a client is echoed by server to ease debugging
+ * Asynchronous chat feature will be incorporated in the future versions.
+ */
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -8,11 +16,39 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 
+void *connection_handler(void *socket_desc)
+{
+  int sock = *(int*)socket_desc;
+  int read_size;
+  char *message,client_message[2000];
+  
+  while((read_size=recv(sock,client_message,2000,0))>0)
+  {
+    write(sock,client_message,strlen(client_message));
+    memset(client_message,'\0',2000);
+  }
+
+  if(read_size==0)
+  {
+    puts("Client disconnected");
+    fflush(stdout);
+  }
+  else if(read_size == -1)
+  {
+    perror("recv failed");
+  }
+
+  close(sock);
+  free(socket_desc);
+
+  return 0;
+}
+
+
 int main(int argc,char *argv[])
 {
-  int c,clientfd,sockfd,read_size;
+  int c,clientfd,sockfd,*new_sock;
   struct sockaddr_in server,client;
-  char client_message[2000];
   sockfd = socket(AF_INET,SOCK_STREAM,IPPROTO_IP);
   if(sockfd<0)
   {
@@ -35,29 +71,30 @@ int main(int argc,char *argv[])
   puts("Listening to incoming connection...\n");
   
   c = sizeof(struct sockaddr_in);
-  clientfd = accept(sockfd,(struct sockaddr*)&client,(socklen_t*)&c);
+  while((clientfd = accept(sockfd,(struct sockaddr*)&client,(socklen_t*)&c)))
+  {
+
+    puts("Connection Accepted!\n");
+   
+    pthread_t sniffer_thread;
+    new_sock = malloc(1);
+    *new_sock = clientfd;
+    
+    //Blocking not handled yet
+    if(pthread_create(&sniffer_thread,NULL,connection_handler,(void*)new_sock)<0)
+    {
+       perror("could not create thread");
+       return 1;
+    }
+    pthread_join(sniffer_thread,NULL);
+    puts("Handler assigned");
+  }
   if(clientfd<0)
   {
     perror("Accept failed!\n");
     return EXIT_FAILURE;
   }
-  puts("Connection Accepted!\n");
   
-  while((read_size = recv(clientfd,client_message,2000,0))>0)
-  {
-    write(clientfd,client_message,strlen(client_message));
-    memset(client_message,'\0',2000);
-  }
-  if(read_size==0)
-  {
-    perror("No input received from client!\n");
-    fflush(stdout);
-  }
-  else if(read_size<0)
-  {
-    perror("recv failed!\n");
-    return EXIT_FAILURE;
-  }
   close(sockfd);
   return EXIT_SUCCESS;
 }
