07ede1a593a01dd985a7ef71d3797c6f67c51d9d
Vignesh Vedavyasan <tovigu@gmail.com>
2019-01-22 23:20:56 +0530

    Additional Comments for understanding in future.

--- Palindrome Problems/maxPalindromeString.c
+++ Palindrome Problems/maxPalindromeString.c
@@ -20,19 +20,41 @@ void findMaximumPalindromicString(char* text) {
   int l[n];
   l[0] = 0;
   l[1] = 1;
-  int maxLPSLength = 0, i=0, diff=-1, R = 2, C = 1, iMirror, start = -1, end = -1;
+  int maxLPSLength = 0;
+  int i=0;//currentRightPosition 
+  int iMirror;//currentLeftPosition 
+  int diff=-1;
+  int R = 2, //centerRightPosition
+  int C = 1;//centerPosition
+  int start = -1, end = -1;
   int maxLPSCenterPosition = 0;
   for(i = 2 ; i < n ; i++ ) {
 
+    /* get currentLeftPosition iMirror for currentRightPosition i
+     */
     iMirror = 2 * C - i;
     l[i] = 0;
     diff = R - i;
+    /* If currentRightPosition i is within centerRightPosition R 
+     */
     if(diff > 0)
       l[i] = min( l[iMirror], diff);
+    
+    /* Attempt to expand palindrome centered at currentRightPosition i 
+     * Here for odd positions, we compare characters and  
+     * if match then increment LPS Length by ONE 
+     * If even position, we just increment LPS by ONE without  
+     * any character comparison 
+     */
     while(((i+l[i]) < n) && ((i-l[i]) > 0) &&
         (((i+l[i]+1)%2 == 0) || (text[(i+l[i]+1)/2] == text[(i-l[i]-1)/2]))) {
       l[i]++;
     }
+
+    /* If palindrome centered at currentRightPosition i
+     * expand beyond centerRightPosition R,
+     * adjust centerPosition C based on expanded palindrome.
+     */
     if(maxLPSLength < l[i]) {
       maxLPSLength = l[i];
       maxLPSCenterPosition = i;
@@ -52,6 +74,7 @@ void findMaximumPalindromicString(char* text) {
   printf("\n");
 }
 
+/* Driver function */
 int main() {
 
   char *text = (char*)malloc(14*sizeof(char));
